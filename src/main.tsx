8dcef121e60659866c52ed9c02f0d4ed4c9c3ced
arturajackowiak <artur.jackowiak@rsqtechnologies.com>
2023-03-20 18:36:48 +0100

    Add styled-components and GlobalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>
 );
