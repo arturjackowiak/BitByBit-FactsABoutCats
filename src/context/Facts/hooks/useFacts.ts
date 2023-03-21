7a4da70495b339b43e6d7eea5b429d0932db1652
arturajackowiak <artur.jackowiak@rsqtechnologies.com>
2023-03-21 13:34:31 +0100

    Optimize useFacts

--- src/context/Facts/hooks/useFacts.ts
+++ src/context/Facts/hooks/useFacts.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import { FactType } from 'Types/FactType';
@@ -6,53 +6,64 @@ import { FactType } from 'Types/FactType';
 const defaultFactsLink = 'https://catfact.ninja/facts?max_length=75';
 
 export const useFacts = () => {
-  const localFacts = JSON.parse(window.localStorage.getItem('facts') ?? '{}');
+  const localFacts = JSON.parse(window.localStorage.getItem('facts') ?? '[]');
   const [facts, setFacts] = useState<FactType[]>(localFacts);
   const [isFetching, setIsFetching] = useState(false);
+  const firstRender = useRef(true);
 
   const deleteFact = useCallback(
     (factId: string) => setFacts((prev) => prev.filter(({ id }) => id !== factId)),
-    [setFacts]
+    [facts, setFacts]
   );
 
   const editFact = useCallback(
     (factId: string, editedFact: FactType) => {
       setFacts((prev) => prev.map((fact) => (fact.id === factId ? editedFact : fact)));
     },
-    [setFacts]
+    [facts, setFacts]
   );
 
   const addFact = useCallback(
     (newFact: FactType) => {
       setFacts((prev) => [...prev, newFact]);
     },
-    [setFacts]
+    [facts, setFacts]
   );
 
-  const fetchFacts = useCallback(async () => {
+  const fetchFacts = async () => {
     setIsFetching(true);
     try {
       const response = await fetch(defaultFactsLink);
-      const cats = await response.json();
-      const factsList = cats.data;
+      const { data } = await response.json();
+      data.forEach((fact: FactType) => ({
+        ...fact,
+        id: nanoid()
+      }));
 
-      factsList.forEach((fact: FactType) => (fact.id = nanoid()));
-
-      setFacts(factsList);
+      setFacts(data);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsFetching(false);
     }
-    setIsFetching(false);
-  }, []);
+  };
 
   useEffect(() => {
-    if (isFetching || Array.isArray(facts)) return;
+    if (!firstRender.current) return;
     fetchFacts();
-  }, [fetchFacts, isFetching, facts]);
+    firstRender.current = false;
+  }, []);
 
   useEffect(() => {
     window.localStorage.setItem('facts', JSON.stringify(facts));
   }, [facts]);
 
-  return { facts, addFact, editFact, deleteFact, isFetching };
+  const memoizedFacts = useMemo(
+    () => ({
+      ...facts
+    }),
+    [facts]
+  );
+
+  return { facts: memoizedFacts, addFact, editFact, deleteFact, isFetching };
 };
